34eb74644fda50e8bdefc4921acef908ee1af94b
Georgy Guminov <gogagum@gmail.com>
2023-02-04 20:22:34 +0300

    Add win build script.

--- .ci/build-project.sh
+++ .ci/build-project.sh
@@ -14,16 +14,10 @@ set -e
 
 BUILD_DIR=${1:-build}
 
-# only pass toolchain file to CMake if Vcpkg is installed
-
 echo "---- build-project.sh ----"
 echo "BUILD_DIR: $BUILD_DIR"
-echo "CC: $CC"
-echo "CXX: $CXX"
-echo "CXXFLAGS: $CXXFLAGS"
-echo "CMAKE_TOOLCHAIN_FILE: $TOOLCHAIN"
 echo "--------------------------"
 
 mkdir "$BUILD_DIR"
-cmake -G Ninja -DCMAKE_BUILD_TYPE=Debug -DCMAKE_TOOLCHAIN_FILE="$TOOLCHAIN" -DUSE_SANITIZER=address -DBUILD_TEST:BOOL=ON -B "$BUILD_DIR"
+cmake -G Ninja -DCMAKE_BUILD_TYPE=Debug -DUSE_SANITIZER=address -DBUILD_TEST:BOOL=ON -B "$BUILD_DIR"
 cmake --build "$BUILD_DIR" -j
