1b411493479792572dcac0023a15932990c3aa95
Georgy Guminov <gogagum@gmail.com>
2024-02-25 02:20:15 +0300

    Update licence time period.

--- test/reference/seg_tree_reference_base.hpp
+++ test/reference/seg_tree_reference_base.hpp
@@ -1,5 +1,5 @@
 ////////////////////////////////////////////////////////////////////////////////
-// Copyright Georgy Guminov 2023.
+// Copyright Georgy Guminov 2023-2024.
 // Distributed under the Boost Software License,
 // Version 1.0. (See accompanying file LICENSE_1_0.txt
 // or copy at https://www.boost.org/LICENSE_1_0.txt)
@@ -9,7 +9,6 @@
 
 #include <algorithm>
 #include <concepts>
-#include <functional>
 #include <ranges>
 #include <vector>
 
