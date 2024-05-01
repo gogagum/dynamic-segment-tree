409dd900eea2b4a5d440dd1b6eb5058f29f6c5f1
Georgy Guminov <gogagum@gmail.com>
2024-05-01 11:03:10 +0300

    Refactor tests.

--- test/test_sum.cpp
+++ test/test_sum.cpp
@@ -26,37 +26,41 @@ using GenerateIndRng = GenerateIndexRange<size_t>;
 // cert-err58-cpp)
 
 TEST(DynamicSumSegmentTree, Construct) {
-  auto tree = DynamicSumSegmentTree<int, int>(0, 42, 54);
+  const auto tree = DynamicSumSegmentTree<int, int>(0, 42, 54);
 }
 
 TEST(DynamicSumSegmentTree, QueryMiddle) {
-  auto tree = DynamicSumSegmentTree<int, int>(0, 42, 54);
+  const auto tree = DynamicSumSegmentTree<int, int>(0, 42, 54);
   EXPECT_EQ(tree.get(21), 54);
 }
 
 TEST(DynamicSumSegmentTree, QueryEnd) {
-  auto tree = DynamicSumSegmentTree<int, int>(0, 42, 54);
+  const auto tree = DynamicSumSegmentTree<int, int>(0, 42, 54);
   EXPECT_THROW(tree.get(42), std::out_of_range);
 }
 
 TEST(DynamicSumSegmentTree, QueryAfterEnd) {
-  auto tree = DynamicSumSegmentTree<int, int>(0, 42, 54);
+  const auto tree = DynamicSumSegmentTree<int, int>(0, 42, 54);
   EXPECT_THROW(tree.get(73), std::out_of_range);
 }
 
 TEST(DynamicSumSegmentTree, QueryBegin) {
-  auto tree = DynamicSumSegmentTree<int, int>(0, 42, 54);
+  const auto tree = DynamicSumSegmentTree<int, int>(0, 42, 54);
   EXPECT_EQ(tree.get(0), 54);
 }
 
 TEST(DynamicSumSegmentTree, QueryBeforeBegin) {
-  auto tree = DynamicSumSegmentTree<int, int>(0, 42, 54);
+  const auto tree = DynamicSumSegmentTree<int, int>(0, 42, 54);
   EXPECT_THROW(tree.get(-5), std::out_of_range);
 }
 
 TEST(DynamicSumSegmentTree, OneUpdate) {
-  auto tree = DynamicSumSegmentTree<int, int, int, std::plus<int>>(0, 42, 54);
-  tree.update(15, 37, 18);
+  const auto tree = [] {
+    auto ret = DynamicSumSegmentTree<int, int, int, std::plus<int>>(0, 42, 54);
+    ret.update(15, 37, 18);
+    return ret;
+  }();
+
   EXPECT_EQ(tree.get(5), 54);
   EXPECT_EQ(tree.get(14), 54);
   EXPECT_EQ(tree.get(15), 54 + 18);
@@ -66,9 +70,13 @@ TEST(DynamicSumSegmentTree, OneUpdate) {
 }
 
 TEST(DynamicSumSegmentTree, TwoSameUpdates) {
-  auto tree = DynamicSumSegmentTree<int, int, int, std::plus<int>>(0, 42, 54);
-  tree.update(15, 37, 18);
-  tree.update(15, 37, 14);
+  const auto tree = [] {
+    auto ret = DynamicSumSegmentTree<int, int, int, std::plus<int>>(0, 42, 54);
+    ret.update(15, 37, 18);
+    ret.update(15, 37, 14);
+    return ret;
+  }();
+
   EXPECT_EQ(tree.get(5), 54);
   EXPECT_EQ(tree.get(14), 54);
   EXPECT_EQ(tree.get(15), 54 + 18 + 14);
@@ -78,9 +86,13 @@ TEST(DynamicSumSegmentTree, TwoSameUpdates) {
 }
 
 TEST(DynamicSumSegmentTree, TwoIntersectingUpdates) {
-  auto tree = DynamicSumSegmentTree<int, int, int, std::plus<int>>(0, 42, 54);
-  tree.update(15, 37, 18);
-  tree.update(20, 41, 14);
+  const auto tree = [] {
+    auto ret = DynamicSumSegmentTree<int, int, int, std::plus<int>>(0, 42, 54);
+    ret.update(15, 37, 18);
+    ret.update(20, 41, 14);
+    return ret;
+  }();
+
   EXPECT_EQ(tree.get(5), 54);
   EXPECT_EQ(tree.get(14), 54);
   EXPECT_EQ(tree.get(15), 54 + 18);
@@ -99,9 +111,13 @@ TEST(DynamicSumSegmentTree, TwoIntersectingUpdates) {
 }
 
 TEST(DynamicSumSegmentTree, TwoNonIntersectingUpdates) {
-  auto tree = DynamicSumSegmentTree<int, int, int, std::plus<int>>(0, 42, 54);
-  tree.update(5, 8, 18);
-  tree.update(17, 38, 14);
+  const auto tree = [] {
+    auto ret = DynamicSumSegmentTree<int, int, int, std::plus<int>>(0, 42, 54);
+    ret.update(5, 8, 18);
+    ret.update(17, 38, 14);
+    return ret;
+  }();
+
   EXPECT_EQ(tree.get(3), 54);
   EXPECT_EQ(tree.get(5), 54 + 18);
   EXPECT_EQ(tree.get(6), 54 + 18);
@@ -116,63 +132,66 @@ TEST(DynamicSumSegmentTree, TwoNonIntersectingUpdates) {
 }
 
 TEST(DynamicSumSegmentTree, SimpleRangeGetAll) {
-  auto tree = DynamicSumSegmentTree<int, int>(0, 42, 54);
-  auto rangeGetResult = tree.rangeGet(0, 42);
+  const auto tree = DynamicSumSegmentTree<int, int>(0, 42, 54);
+  const auto rangeGetResult = tree.rangeGet(0, 42);
   EXPECT_EQ(rangeGetResult, 54 * 42);
 }
 
 TEST(DynamicSumSegmentTree, SimpleRangeGetPart) {
-  auto tree = DynamicSumSegmentTree<int, int>(0, 42, 54);
-  auto rangeGetResult = tree.rangeGet(12, 23);
+  const auto tree = DynamicSumSegmentTree<int, int>(0, 42, 54);
+  const auto rangeGetResult = tree.rangeGet(12, 23);
   EXPECT_EQ(rangeGetResult, 54 * (23 - 12));
 }
 
 TEST(DynamicSumSegmentTree, SimpleRangeGetMoreThanAll) {
-  auto tree = DynamicSumSegmentTree<int, int>(0, 42, 54);
-  auto rangeGetResult = tree.rangeGet(-5, 50);
+  const auto tree = DynamicSumSegmentTree<int, int>(0, 42, 54);
+  const auto rangeGetResult = tree.rangeGet(-5, 50);
   EXPECT_EQ(rangeGetResult, 54 * 42);
 }
 
 TEST(DynamicSumSegmentTree, RangeGetAfterUpdate) {
-  auto tree = DynamicSumSegmentTree<int, int, int, std::plus<int>>(0, 42, 54);
-  tree.update(15, 37, 18);
+  const auto tree = [] {
+    auto ret = DynamicSumSegmentTree<int, int, int, std::plus<int>>(0, 42, 54);
+    ret.update(15, 37, 18);
+    return ret;
+  }();
 
-  auto getInsideUpdateRes = tree.rangeGet(17, 30);
+  const auto getInsideUpdateRes = tree.rangeGet(17, 30);
   EXPECT_EQ(getInsideUpdateRes, (54 + 18) * (30 - 17));
 
-  auto getOfUpdateSizeRes = tree.rangeGet(15, 37);
+  const auto getOfUpdateSizeRes = tree.rangeGet(15, 37);
   EXPECT_EQ(getOfUpdateSizeRes, (54 + 18) * (37 - 15));
 
-  auto getGreaterThanUpdateSizeRes = tree.rangeGet(5, 40);
+  const auto getGreaterThanUpdateSizeRes = tree.rangeGet(5, 40);
   EXPECT_EQ(getGreaterThanUpdateSizeRes, 54 * (40 - 5) + 18 * (37 - 15));
 
-  auto intersectingGet1Res = tree.rangeGet(5, 30);
+  const auto intersectingGet1Res = tree.rangeGet(5, 30);
   EXPECT_EQ(intersectingGet1Res, 54 * (30 - 5) + 18 * (30 - 15));
 
-  auto intersectingGet2Res = tree.rangeGet(30, 40);
+  const auto intersectingGet2Res = tree.rangeGet(30, 40);
   EXPECT_EQ(intersectingGet2Res, 54 * (40 - 30) + 18 * (37 - 30));
 }
 
 TEST(DynamicSumSegmentTree, LongLongToInt) {
-  auto tree = DynamicSumSegmentTree<long long, int>(-100500, 100500, 42);
+  const auto tree = DynamicSumSegmentTree<long long, int>(-100500, 100500, 42);
 
-  auto testVal = tree.get(17);
+  const auto testVal = tree.get(17);
   EXPECT_EQ(testVal, 42);
 }
 
 TEST(DynamicSumSegmentTree, IntToLong) {
-  auto tree = DynamicSumSegmentTree<int, long long, long long>(
+  const auto tree = DynamicSumSegmentTree<int, long long, long long>(
       -100500, 100500, 1234567890098765432);
 
-  auto testVal = tree.get(17);
+  const auto testVal = tree.get(17);
   EXPECT_EQ(testVal, 1234567890098765432);
 }
 
 TEST(DynamicSumSegmentTree, LongLongToLongLong) {
-  auto tree = DynamicSumSegmentTree<long long, long long>(-100500, 100500,
-                                                          1234567890098765432);
+  const auto tree = DynamicSumSegmentTree<long long, long long>(
+      -100500, 100500, 1234567890098765432);
 
-  auto testVal = tree.get(17);
+  const auto testVal = tree.get(17);
   EXPECT_EQ(testVal, 1234567890098765432);
 }
 
@@ -180,26 +199,34 @@ TEST(DynamicSumSegmentTree, LongLongToLongLongLadder) {
   const auto topBorder = 1000000ll;
   using SegTree = DynamicSumSegmentTree<long long, long long, long long,
                                         std::plus<long long>>;
-  auto tree = SegTree(0ll, topBorder);
+  const auto tree = [] {
+    auto ret = SegTree(0ll, topBorder);
 
-  tree.update(topBorder - topBorder / 2, topBorder, 1ll);
-  tree.update(topBorder - topBorder / 4, topBorder, 10ll);
-  tree.update(topBorder - topBorder / 8, topBorder, 100ll);
-  tree.update(topBorder - topBorder / 16, topBorder, 1000ll);
-  tree.update(topBorder - topBorder / 32, topBorder, 10000ll);
-  tree.update(topBorder - topBorder / 64, topBorder, 100000ll);
-  tree.update(topBorder - topBorder / 128, topBorder, 1000000ll);
-  tree.update(topBorder - topBorder / 256, topBorder, 10000000ll);
+    ret.update(topBorder - topBorder / 2, topBorder, 1ll);
+    ret.update(topBorder - topBorder / 4, topBorder, 10ll);
+    ret.update(topBorder - topBorder / 8, topBorder, 100ll);
+    ret.update(topBorder - topBorder / 16, topBorder, 1000ll);
+    ret.update(topBorder - topBorder / 32, topBorder, 10000ll);
+    ret.update(topBorder - topBorder / 64, topBorder, 100000ll);
+    ret.update(topBorder - topBorder / 128, topBorder, 1000000ll);
+    ret.update(topBorder - topBorder / 256, topBorder, 10000000ll);
 
-  auto testVal1 = tree.get(topBorder - 1);
+    return ret;
+  }();
+
+  const auto testVal1 = tree.get(topBorder - 1);
   EXPECT_EQ(testVal1, 11111111);
-  auto testVal2 = tree.get(topBorder - topBorder / 32 - 1);
+  const auto testVal2 = tree.get(topBorder - topBorder / 32 - 1);
   EXPECT_EQ(testVal2, 1111);
 }
 
 TEST(DynamicSumSegmentTree, Set) {
-  auto tree = DynamicSumSegmentTree<int, int>(0, 42, 21);
-  tree.set(0, 37, 73);
+  const auto tree = [] {
+    auto ret = DynamicSumSegmentTree<int, int>(0, 42, 21);
+    ret.set(0, 37, 73);
+    return ret;
+  }();
+
   EXPECT_EQ(tree.get(0), 73);
   EXPECT_EQ(tree.get(15), 73);
   EXPECT_EQ(tree.get(37), 21);
@@ -207,9 +234,13 @@ TEST(DynamicSumSegmentTree, Set) {
 }
 
 TEST(DynamicSumSegmentTree, SetOnTheSameRange) {
-  auto tree = DynamicSumSegmentTree<int, int>(0, 42, 21);
-  tree.set(0, 37, 73);
-  tree.set(0, 37, 37);
+  const auto tree = [] {
+    auto ret = DynamicSumSegmentTree<int, int>(0, 42, 21);
+    ret.set(0, 37, 73);
+    ret.set(0, 37, 37);
+    return ret;
+  }();
+
   EXPECT_EQ(tree.get(0), 37);
   EXPECT_EQ(tree.get(15), 37);
   EXPECT_EQ(tree.get(37), 21);
@@ -219,9 +250,12 @@ TEST(DynamicSumSegmentTree, SetOnTheSameRange) {
 TEST(DynamicSumSegmentTree, IntRangesGiveInt64Sum) {
   constexpr auto maxInt = std::numeric_limits<int>::max();
 
-  auto tree = DynamicSumSegmentTree<int, int, int64_t>(0, 3, 0);
-  tree.set(0, 1, maxInt - 13);
-  tree.set(1, 3, maxInt - 7);
+  const auto tree = [] {
+    auto ret = DynamicSumSegmentTree<int, int, int64_t>(0, 3, 0);
+    ret.set(0, 1, maxInt - 13);
+    ret.set(1, 3, maxInt - 7);
+    return ret;
+  }();
 
   const auto part1LL = static_cast<int64_t>(maxInt - 13);
   const auto part2LL = static_cast<int64_t>(maxInt - 7);
@@ -232,9 +266,12 @@ TEST(DynamicSumSegmentTree, IntRangesGiveInt64Sum) {
 TEST(DynamicSumSegmentTree, IntRangesGiveInt64SumLongRange) {
   constexpr auto maxInt = std::numeric_limits<int>::max();
 
-  auto tree = DynamicSumSegmentTree<int, int, int64_t>(0, 100, 0);
-  tree.set(0, 42, maxInt / 3);
-  tree.set(42, 100, maxInt / 7);
+  const auto tree = [] {
+    auto ret = DynamicSumSegmentTree<int, int, int64_t>(0, 100, 0);
+    ret.set(0, 42, maxInt / 3);
+    ret.set(42, 100, maxInt / 7);
+    return ret;
+  }();
 
   const auto part1LL = static_cast<int64_t>(maxInt / 3);
   const auto part2LL = static_cast<int64_t>(maxInt / 7);
